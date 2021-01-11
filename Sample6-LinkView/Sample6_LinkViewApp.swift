7fe6950741d3a7593cc674e9b68d938295f2d1ea
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-11 09:37:45 +0900

    Initial Commit

---
+++ Sample6-LinkView/Sample6_LinkViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample6_LinkViewApp.swift
+//  Sample6-LinkView
+//
+//  Created by keiji yamaki on 2021/01/11.
+//
+
+import SwiftUI
+
+@main
+struct Sample6_LinkViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
